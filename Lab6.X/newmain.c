8e2e36d715598fe02ffdc2dd609587d16071a8b6
Zak <z.catherall@2016.hull.ac.uk>
2019-11-01 16:36:15 +0000

    Added example thermo code. not working

--- Lab6.X/newmain.c
+++ Lab6.X/newmain.c
@@ -10,17 +10,30 @@
 #define rw RA4
 #define e RA3
 #define psb RA2
-#define dq RA0
-#define dq_dir TRISA0
+#define rst RA0
+#define dq RB0
+#define dq_dir TRISB0
 #define set_dq_high() dq_dir = 1
 #define set_dq_low() dq = 0; dq_dir = 0
-#define nop() asm("nop") // 1us delay.
 
-// Timings for delay2 args.
-const int t503us[2] = {2, 70};
-const int t430us[2] = {2, 60};
-const int t70us[2] = {2, 8};
-const int t63us[2] = {2, 7};
+// Timings for delay2 args. Cannot be const.
+int t503us[2] = {2, 70};
+int t430us[2] = {2, 60};
+int t70us[2] = {2, 8};
+int t63us[2] = {2, 7};
+int tUKus[2] = {10, 70};
+
+//
+unsigned char TLV = 0; //temperature high byte                     
+unsigned char THV = 0; //temperature low byte                      
+unsigned char TZ = 0; //temperature integer after convert         
+unsigned char TX = 0; //temperature decimal  after convert        
+unsigned int wd; //temperature BCD code  after convert       
+
+// Temperature component vars?
+unsigned char tens, intEntries, pt10, pt100, pt1000, pt10000;
+unsigned char table[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+//the display code of 0-9     
 
 //
 void init(); //I/O init
@@ -28,7 +41,7 @@ void lcd_init(); //LCD init
 void writecmd(char x); //display one byte
 void writechar(char x); //LCD display setting.
 void delay(); //delay
-void delay2(char arr[]);
+void delay2(int arr[]);
 
 //I/O init
 
@@ -43,6 +56,7 @@ void init() {
 //LCD init
 
 void lcd_init() {
+    rst = 0;
     writecmd(0x0F); //display on,cursor on,blink on.
     writecmd(0x01); //clr screen
     writecmd(0x38); // 8 bits 2 lines 5*7 mode. / Set function
@@ -131,24 +145,154 @@ void delay2(int t[]) {
     } while (--x);
 }
 
-void write_byte() {
+void write_byte(unsigned char val) {
+    unsigned char i;
+    unsigned char temp;
 
+    // Iterate through bits from l to r.
+    for (i = 8; i > 0; i--) {
+        temp = val & 0x01; // Shift lowest bit.
+        set_dq_low();
+        NOP();
+        NOP();
+        NOP();
+        NOP();
+        NOP();
+        //        NOP();
+        if (temp == 1) {
+            set_dq_high();
+        }
+        delay2(t63us);
+        set_dq_high();
+        NOP();
+        NOP();
+        val = val >> 1; // right shift bit.
+    }
 }
 
-void read_byte() {
+unsigned char read_byte() {
+    unsigned char i;
+    unsigned char value = 0; //read temperature         
+    static char j;
+    //    static int j;
 
+    for (i = 8; i > 0; i--) {
+        value >>= 1;
+        set_dq_low();
+        NOP();
+        NOP();
+        NOP();
+        NOP();
+        NOP();
+        NOP(); //6us              
+        set_dq_high(); // pull high       
+        NOP();
+        NOP();
+        NOP();
+        NOP();
+        NOP(); //4us               
+        j = dq;
+        if (j) value |= 0x80; // ????
+        delay2(t63us); //63us              
+    }
+    return (value);
 }
 
+void display_temp() {
+    TRISA = 0X00; //set A PORT all OUTPUT                          
+    PORTD = table[tens]; //display integer ten bit                        
+    PORTA = 0x3e;
+    delay2(tUKus);
+    PORTD = table[intEntries]&0X7F; //display integer Entries bit and decimal dot    
+    PORTA = 0x3d;
+    delay2(tUKus);
+    PORTD = table[pt10]; //display decimal ten cent bit                   
+    PORTA = 0x3b;
+    delay2(tUKus);
+    PORTD = table[pt100]; //display decimal hundred cent bit               
+    PORTA = 0x37;
+    delay2(tUKus);
+    PORTD = table[pt1000]; //display decimal thousand  cent bit             
+    PORTA = 0x2f;
+    delay2(tUKus);
+    PORTD = table[pt10000]; //display decimal myriad cent bit                
+    PORTA = 0x1f;
+    delay2(tUKus);
+}
 
+reset(void) {
+    char presence = 1;
+    while (presence) {
+        set_dq_low(); // MCU pull low
+        delay2(t503us);
+        set_dq_high(); // release general line and wait for pull high
+        delay2(t70us);
+        if (dq == 1) {
+            presence = 1; // Didn't receive response.
+        } else {
+            presence = 0; // Received response.
+        }
+        delay2(t430us);
+    }
+}
+
+void get_temp() {
+    //The sequence has to be
+    // followed by each transaction: 1 Initialisation -> 2 ROM Function Command -> 3Memory
+    // Function Command -> 4 Transaction/Data
+
+    int i;
+    set_dq_high();
+    reset(); //reset,wait for  18b20 response.                                                                                                              
+    write_byte(0XCC); //ignore ROM matching                                                                                                                            
+    write_byte(0X44); //send  temperature convert command                                                                                                              
+    for (i = 20; i > 0; i--) {
+        display_temp(); //call some display function,insure the time of convert temperature                                                                              
+    }
+    reset(); //reset again,wait for 18b20 response                                                                                                        
+    write_byte(0XCC); //ignore ROM matching                                                                                                                            
+    write_byte(0XBE); //send read temperature command                                                                                                                  
+    TLV = read_byte(); //read temperature low byte                                                                                                                      
+    THV = read_byte(); //read temperature high byte                                                                                                                     
+    set_dq_high(); //release general line                                                                                                                           
+    TZ = (TLV >> 4) | (THV << 4)&0X3f; //temperature integer                                                                                                                            
+    TX = TLV << 4; //temperature decimal                                                                                                                            
+    if (TZ > 100) {
+        TZ / 100; //not display hundred bit // ISN'T DOING ANYTHING????
+    }
+    intEntries = TZ % 10; //integer Entries bit                                                                                                                            
+    tens = TZ / 10; //integer ten bit                                                                                                                                
+    wd = 0;
+    if (TX & 0x80) {
+        wd = wd + 5000;
+    }
+    if (TX & 0x40) {
+        wd = wd + 2500;
+    }
+    if (TX & 0x20) {
+        wd = wd + 1250;
+    }
+    if (TX & 0x10) {
+        wd = wd + 625; //hereinbefore four instructions are turn  decimal into BCD code        
+    }
+    pt10 = wd / 1000; //ten cent bit                                                                           
+    pt100 = (wd % 1000) / 100; //hundred cent bit                                                                       
+    pt1000 = (wd % 100) / 10; //thousand cent bit                                                                      
+    pt10000 = wd % 10; //myriad cent bit                                                                        
+    NOP();
+}
 
 //main
 
 void main() {
+    init(); //call system initialize function                                                                                                                                 
+
     while (1) {
-        // Display temperature on LCD.
-        
-        //The sequence has to be
-        // followed by each transaction: 1 Initialization -> 2 ROM Function Command -> 3Memory
-        // Function Command -> 4 Transaction/Data
+//        get_temp(); //call temperature convert function                                                                                                                                   
+//        display_temp(); //call display function                                                                                                                                           
+
+        char *str = "Hello";
+        setCursorPos(1, 2);
+        writeString(str);
     }
 }
