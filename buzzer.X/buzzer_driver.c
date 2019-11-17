46dcb32105bfd4be31a056f7fafd4f1be334150d
Zak (Desktop) <zakcatherall@outlook.com>
2019-11-17 18:21:59 +0000

    Created standalone buzzer project

---
+++ buzzer.X/buzzer_driver.c
@@ -0,0 +1,13 @@
+/*
+ * File:   buzzer_driver.c
+ * Author: Zak
+ *
+ * Created on 17 November 2019, 18:17
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ buzzer.X/buzzer_driver.h
@@ -0,0 +1,15 @@
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_BUZZER_H
+	#define	XC_BUZZER_H
+	
+	#include <xc.h> // include processor files - each processor file is guarded.  
+//	void writecmd(char command);
+//	void writechar(char character);
+//	void writeInt(int i);
+//	void writeString(char str[])
+//	void setCursorPos(int lineN, int pos);
+//	void lcd_init(void);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
