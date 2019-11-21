6c67ba02436fe237152a01cc73ed4eba9b569eba
ItDidntCatchFire <deaddanster@googlemail.com>
2019-11-21 14:04:15 +0000

    Added Matrix files

---
+++ ACW.X/matrix.h
@@ -0,0 +1,8 @@
+// This is a guard condition so that contents of this file are not included more than once.  
+#ifndef XC_Matrix_H
+	#define	XC_Matrix_H
+	
+	#include <xc.h> // include processor files - each processor file is guarded.  
+	    
+#endif	/* XC_Matrix_H */
+    
